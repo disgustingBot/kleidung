d01444f1ea05e67623edad76a90c68b9bde1b449
Rafael Molinero <rafmoline@gmail.com>
2020-03-18 14:19:12 +0100

    more Stories

--- single-post.php
+++ single-post.php
@@ -4,47 +4,64 @@
 
 <main class="singleStories">
 
-<section class="singleStorie">
+  <section class="singleStorie">
 
-  <figure class="singleStorieFigure">
-    <img class="singleStorieImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-    <button class="sliderArrow" type="button" name="button">&#62;</button>
-    <button class="sliderArrow slideLeft" type="button" name="button">&#62;</button>
-  </figure>
+    <figure class="singleStorieFigure">
+      <img class="singleStorieImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+      <button class="sliderArrow" type="button" name="button">&#62;</button>
+      <button class="sliderArrow slideLeft" type="button" name="button">&#62;</button>
+    </figure>
 
 
-</section>
-<section class="singleStorieCaption">
-  <h4 class="storieDate">
-    <?php echo get_the_date(); ?>
-  </h4>
-  <h3 class="singleStorieTitle">
-    <?php echo the_title(); ?>
-  </h3>
-  <p class="singleStorieContent">
-    <?php echo the_content(); ?>
-  </p>
-</section>
+  </section>
+  <section class="singleStorieCaption">
+    <h4 class="storieDate">
+      <?php echo get_the_date(); ?>
+    </h4>
+    <h3 class="singleStorieTitle">
+      <?php echo the_title(); ?>
+    </h3>
+    <p class="singleStorieContent">
+      <?php echo the_content(); ?>
+    </p>
+  </section>
+  <!--
+  ////////////////////
+                  SECCION SOCIAL SHARING
+  ////////////////////
+  -->
+
+  <section class="storieSocialSharing">
+    <h5 class="storieSocialSharingTitle">SHARE</h5>
+    <?php include 'socialMedia.php'; ?>
+  </section>
+
+
 
   <!--
   ////////////////////
                   SECCION OTRAS ENTRADAS
-                  ////////////////////
+  ////////////////////
   -->
+  <h4 class="singleOthersTitle">
+    OTRAS ENTRADAS
+  </h4>
 
   <section class="singleStorieOthers">
-    <h4 class="singleOthersTitle">
-      OTRAS ENTRADAS
-    </h4>
     <?php $args = array('post_type' => 'post',
-    'posts_per_page' => 2, );
+    'posts_per_page' => 4, );
     $stories=new WP_Query($args);
     while($stories->have_posts()){$stories->the_post(); ?>
-      <figure class="storieCard">
+      <figure class="card">
         <a class="storieLink" href="<?php the_permalink(); ?>">
-          <img class="storieImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+          <img class="cardImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
         </a>
-        <figcaption class="storieTitle">
+        <p class="cardStorieDate">
+          <?php echo get_the_date( 'j' ); ?>
+          <br>
+          <?php echo get_the_date( 'M' ); ?>
+        </p>
+        <figcaption class="cardCaption">
           <h3>
             <?php the_title(); ?>
           </h3>

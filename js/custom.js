5897aef05d9602264f5f98cdd1e03ce76057a689
tomasmparra <tomas.mparra@lattedev.com>
2020-03-18 14:08:40 +0100

    summary (required)

--- js/custom.js
+++ js/custom.js
@@ -108,3 +108,11 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+// LOGO ANIMATION
+const logo = document.querySelectorAll("#logo path");
+
+for(let i = 0; i<logo.length; i++) {
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}

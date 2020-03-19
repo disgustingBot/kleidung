871199f3417be2763b97064d91bcae6e91bdebdc
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2020-03-19 17:27:34 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -123,6 +123,6 @@ for (i = 0; i < acc.length; i++) {
 // LOGO ANIMATION
 const logo = document.querySelectorAll("#logo path");
 
-for(let i = 0; i<logo.length; i++) {
-  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
+// for(let i = 0; i<logo.length; i++) {
+//   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }

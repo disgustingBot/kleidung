21fdea74441053b7a505397e153c82964ddcb5b1
tomasmparra <tomas.mparra@lattedev.com>
2020-03-19 19:22:08 +0100

    dasd

--- js/custom.js
+++ js/custom.js
@@ -110,9 +110,9 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-// LOGO ANIMATION
-const logo = document.querySelectorAll("#logo path");
-
-for(let i = 0; i<logo.length; i++) {
-  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
+// // LOGO ANIMATION
+// const logo = document.querySelectorAll("#logo path");
+//
+// for(let i = 0; i<logo.length; i++) {
+//   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }

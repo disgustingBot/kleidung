bc8b08bb3cf5cfb65d350d7e0d4d28c1752778ad
tomasmparra <tomas.mparra@lattedev.com>
2020-06-11 14:50:40 +0200

    asdf

--- functions.php
+++ functions.php
@@ -289,8 +289,9 @@ add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 function new_loop_shop_per_page( $cols ) {
   // $cols contains the current number of products per page based on the value stored on Options -> Reading
   // Return the number of products you wanna show per page.
-	$cols = 4;
+	// $cols = 4;
   // $cols = 12;
+	$cols = 24;
   return $cols;
 }
 
